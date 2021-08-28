ffdb1ab53b0a0ca9b2585b3ccff7216dcad62fe7
Jerzor <jerzor2000@vp.pl>
2021-08-28 21:09:54 +0200

    add control functions and states

--- src/App.js
+++ src/App.js
@@ -1,10 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { commerce } from "./lib/commerce";
+import { Navbar, Cart, Products } from "./components";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  // ======== fetch data ========
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
+
+  // ======== handlers ========
+  const handleAddToCart = (productId) => {
+    commerce.cart
+      .add(productId, 1)
+      .then((item) => {
+        setCart(item.cart);
+      })
+      .catch((error) => {
+        console.error("There was an error adding the item to the cart", error);
+      });
+  };
+
+  const handleUpdateCartQty = (lineItemId, quantity) => {
+    commerce.cart
+      .update(lineItemId, { quantity })
+      .then((resp) => {
+        setCart(resp.cart);
+      })
+      .catch((error) => {
+        console.log("There was an error updating the cart items", error);
+      });
+  };
+
+  const handleRemoveFromCart = (lineItemId) => {
+    commerce.cart
+      .remove(lineItemId)
+      .then((resp) => {
+        setCart(resp.cart);
+      })
+      .catch((error) => {
+        console.error(
+          "There was an error removing the item from the cart",
+          error
+        );
+      });
+  };
+
   return (
-    <div>
-      <p>test</p>
-    </div>
+    <Router>
+      <Navbar cartQnt={cart.line_items} />
+      <Switch>
+        <Route path="/" exact>
+          <Products products={products} handleAddToCart={handleAddToCart} />
+        </Route>
+        <Route path="/cart">
+          <Cart
+            cart={cart}
+            handleUpdateCartQty={handleUpdateCartQty}
+            handleRemoveFromCart={handleRemoveFromCart}
+          />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
