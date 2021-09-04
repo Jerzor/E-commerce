f0af4667d2b2bfa9f488b2de1ace2f7ecde00e8f
Jerzor <jerzor2000@vp.pl>
2021-09-04 14:10:17 +0200

    export default Confirmation file

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Navbar } from "./Navbar/Navbar";
 export { default as Cart } from "./Cart/Cart";
 export { default as Products } from "./Products/Products";
 export { default as Checkout } from "./Checkout/Checkout";
+export { default as Confirmation } from "./Confirmation/Confirmation";
